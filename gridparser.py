18cc908d52cf5e70fe07eb09f026b748d2402dea
Max <mskvarci@gmail.com>
2023-01-02 21:01:42 -0500

    commit

--- gridparser.py
+++ gridparser.py
@@ -1,24 +1,58 @@
+# json for loading JSON data
 import json
+# numby for 3 dimensional array support
+import numpy as np
 
+# open and load the JSON file
 f = open('housetest.json')
-
 data = json.load(f)
 
-#for i in data['blocks']:
-    # print("X = " + str(i['pos'][0]) + ", " + "Y = " + str(i['pos'][1]) + ", " "Z = " + str(i['pos'][2]))
+# load block array dimensions
+sizeX = data['size'][0]
+sizeY = data['size'][1]
+sizeZ = data['size'][2]
 
- #   x = str(i['pos'][0])
- # y = str(i['pos'][1])
-  #  z = str(i['pos'][2])
+# initialize 3 dimensional array using limits
+arayBlocks = np.zeros((sizeX,sizeY,sizeZ),int)
 
-def slicer(y):
+# load the array by iterating through the JSON file
+def loadBlocks():
     for i in data['blocks']:
-        #print(str(i['pos'][1]))
-        #print("y = " + str(y))
-        if str(i['pos'][1]) == str(y):
-            print("State = " + str(i['state']))
+        # print("loading:" + str(i['pos'][0]) + " " + str(i['pos'][1]) + " " + str(i['pos'][2]) + "=" + str(i['state']))
+        arayBlocks[int(i['pos'][0]),int(i['pos'][1]),int(i['pos'][2])] = str(i['state'])
 
-slicer(2)
+# Dray out a layer (z)
+def drawLayer(y):
 
+    rowstr = ""
+
+    x = 0
+    z = 0
+    # loop for each row in the layer
+    while z  < sizeZ:
+        #print ("row = " + str(z))
+        # loop for each iten in the row
+        while x < sizeX:
+            rowstr = str(rowstr) + " " + str(arayBlocks[x,y,z])
+            x += 1
+        print(str(rowstr))
+        rowstr = ""
+            
+        # start a the first item of the next row
+        x = 0
+        # move to the next row
+        z += 1
+
+# Load block array
+loadBlocks()
+
+# Draw all layers
+y = 0
+while y < sizeY:
+    print("drawing layer " + str(y))
+    drawLayer(y)
+    y += 1
 
 f.close()
+
+#str(x) + " " + str(y) + " " + str(z) + "=" + 
